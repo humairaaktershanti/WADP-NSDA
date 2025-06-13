61007b8004dbfb5378e4a46af835975e4d664965
Humaira Akter <humairaaktershanti@gmail.com>
2025-06-13 12:54:39 +0600

    before namaz

--- House Prectice/To-Do Model/todo_project/todo_project/views.py
+++ House Prectice/To-Do Model/todo_project/todo_project/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from tasksApp.models import *
 
 
 def home(R):
@@ -6,9 +7,27 @@ def home(R):
 
 
 def addTask(R):
+    if R.method == "POST":
+        title = R.POST.get('title')
+        description=R.POST.get('description')
+        status = R.POST.get('status')
+        due_date = R.POST.get('due_date')
+
+        new_obj = ToDoModel(
+            title = title,
+            description = description,
+            status = status,
+            due_date = due_date,         
+        )
+        new_obj.save()
+
     return render(R,"addTask.html")
 
 
 def taskList(R):
-    return render(R,"taskList.html")
-    
+    listData = ToDoModel.objects.all()
+    context = {
+        'listData': listData
+    }
+
+    return render(R,"taskList.html", context)
